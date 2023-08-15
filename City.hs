131b26e3b85319e2fd143f975b59b5a7b9aca996
Santiago Groba <santiago.groba@gmail.com>
2023-08-15 11:20:02 -0300

    hice hasta el modulo Quality

--- City.hs
+++ City.hs
@@ -6,5 +6,10 @@ import Point
 data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
+newC name point = Cit name point
+
 nameC :: City -> String
+nameC (Cit name _) = name
+
 distanceC :: City -> City -> Float
+distanceC (Cit _ point1) (Cit _ point2) = difP point1 point2
