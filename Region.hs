4d460c8ba070ad43a660defd9e6a443cb237132c
Santiago Groba <santiagogroba@hotmail.com>
2023-08-27 11:25:57 -0300

    region casi terminado

--- Region.hs
+++ Region.hs
@@ -1,6 +1,6 @@
 module Region ( Region, newR, foundR, citiesR, tunelR, pathR , linksR, linkR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR)
    where
-
+import Data.List (find)
 import City
 import Tunel
 import Link
@@ -24,11 +24,33 @@ linksR (Reg _ ls _) = ls
 linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
 linkR (Reg cs ls ts) c1 c2 q = Reg cs (newL c1 c2 q:ls) ts
 
---tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región, buscando una ruta, mediante tuneles, que las conecte. por ejemplo A -> B, B->C , el tunel conecta a A con C.
+tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región, buscando una ruta, mediante tuneles, que las conecte. por ejemplo A -> B, B->C , el tunel conecta a A con C.
+tunelR (Reg cs ls ts) [c1, c2] = Reg cs ls (pathR (Reg cs ls ts) [c1, c2]:ts)
 
+pathR :: Region -> City -> City -> [Link] -- indica el camino de enlaces que hay que recorrer para ir de una ciudad a otra. por ejemplo A -> B, B->C , el tunel conecta a A con C.
+pathR (Reg cs ls _) c1 c2 | c1 == c2 = []
+                           | linkedR (Reg cs ls ts) c1 c2 = findPath (Reg cs ls _) c1 c2 [] []
+                           | otherwise = error "Las ciudades nombradas no se encuentran enlazadas"
+                  
+findPath :: Region -> City -> City -> [City] -> [Link] -> [Link]
+findPath region@(Reg cs ls _) c1 c2 visited path =
+    case ls of
+        [] -> []
+        (Link from to _ : restLinks) ->
+            if from == c1 && to == c2
+                then reverse (path ++ [Link from to (Qua "" 0 0)])
+                else exploreNextCities from restLinks
 
---pathR :: Region -> City -> City -> [Link] -- indica el camino de enlaces que hay que recorrer para ir de una ciudad a otra. por ejemplo A -> B, B->C , el tunel conecta a A con C.
-
+exploreNextCities :: City -> [Link] -> [Link]
+exploreNextCities _ [] = []
+exploreNextCities from (Link nextFrom nextTo _ : restLinks)
+    | nextTo `elem` visited = exploreNextCities from restLinks
+    | otherwise = 
+        let newPath = reverse path ++ [Link from nextTo (Qua "" 0 0)]
+            foundPath = findPath (Reg cs [] []) nextTo c2 (from : visited) newPath
+        in if null foundPath
+              then exploreNextCities from restLinks
+              else foundPath
 
 linksForR :: Region -> City -> [Link] -- indica los enlaces que salen de una ciudad
 linksForR (Reg cs ls ts) c = filter (\l -> connectsL c l) ls
