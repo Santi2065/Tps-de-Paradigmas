ca8fac14f261b10c2e83d3f95829a6fc8c827e01
Santiago Groba <santiagogroba@hotmail.com>
2023-09-01 09:09:43 -0300

    Ubicado en package queue

--- tp2/queue/QueueTest.java
+++ tp2/queue/QueueTest.java
@@ -1,10 +1,11 @@
-package queue;
+package queue.queue;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
+import queue.queue.Queue;
 
 public class QueueTest {
 
