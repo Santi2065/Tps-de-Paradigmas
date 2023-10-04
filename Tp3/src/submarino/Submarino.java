b786113eed769e11dd89e751bfa50b11017a455c
Augusto Lanzini <augusto.lanzini.ar@gmail.com>
2023-10-04 09:29:20 -0300

    Creando el Tp3

---
+++ Tp3/src/submarino/Submarino.java
@@ -0,0 +1,10 @@
+
+package submarino;
+
+public class Submarino {
+	
+	
+	
+	
+	
+}
---
+++ Tp3/src/submarino/subTest.java
@@ -0,0 +1,16 @@
+
+package submarino;
+
+public class subTest {
+	
+	private Submarino subma;
+	
+	@Test
+	public void Setup() {
+		subma = new Submarino();
+	}
+	
+	
+	
+	
+}
