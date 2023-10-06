3e4525ce74d9e55cd1034b796ecbce06a559148a
Augusto Lanzini <augusto.lanzini.ar@gmail.com>
2023-10-06 09:18:17 -0300

    un poco de codigo y tests

--- Tp3/src/submarino/subTest.java
+++ Tp3/src/submarino/subTest.java
@@ -1,15 +1,28 @@
 
 package submarino;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class subTest {
 	
 	private Submarino subma;
+	private SubPosition pos_nula;
+	
+	@BeforeEach
+	subma = new Submarino();
+	
 	
 	@Test
 	public void Setup() {
-		subma = new Submarino();
+		
 	}
 	
+	@Test
+	public void Test01SubmarinoTienePosicionInicial() {
+		assertEquals(pos_nula,subma.position());
+	}
 	
 	
 	
